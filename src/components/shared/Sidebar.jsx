8c5d71a1cd7632a9a871a809041f334859ed9f06
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-06-03 02:49:20 -0300

    debug sidebar with bg

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -12,90 +12,84 @@ import { CiDiscount1 } from "react-icons/ci";
 const Sidebar = (props) => {
 
   // eslint-disable-next-line react/prop-types
-  // eslint-disable-next-line react/prop-types
   const {showMenu} = props;
 
   return (
-    <div 
-    className={`bg-[#1F1D2B] fixed top-0 lg:left-0 w-24 h-full flex flex-col justify-between py-6 rounded-r-xl transition-all
-    ${showMenu ? 'left-0' : '-left-full' } 
-    `}>
-      <div>
-        <ul className="pl-2">
-          {/* Logo */}
-          <li>
-            <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
-              {" "}
-              Logo{" "}
-            </h1>
-          </li>
-          {/* Home */}
-          <li className="p-4 rounded-l-xl bg-[#262837]">
-            <a href="#" className="flex p-4 rounded-xl bg-[#ec7c6a] text-white">
-              <RiHome6Line className="text-2xl justify-center" />
-            </a>
-          </li>
-          {/* Offer */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <CiDiscount1 className="text-2xl justify-center " />
-            </a>
-          </li>
-          {/* Pie */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <RiPieChartLine className="text-2xl justify-center " />
-            </a>
-          </li>
-          {/* Mail */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <RiMailLine className="text-2xl justify-center " />
-            </a>
-          </li>
-          {/* Notification */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <RiNotification2Line className="text-2xl justify-center " />
-            </a>
-          </li>
-          {/* Setting */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <RiSettings5Line className="text-2xl justify-center " />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul className="pl-2">
-          {/* Logout */}
-          <li className="p-4 rounded-l-xl hover:bg-[#262837] group transition-colors">
-            <a
-              href="#"
-              className="flex p-4 rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors"
-            >
-              <RiLogoutBoxRLine className="text-2xl justify-center " />
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div
+    className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 transition-all ${
+      showMenu ? "left-0" : "-left-full"
+    }`}
+  >
+    <div>
+      <ul className="pl-4">
+        <li>
+          <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
+            Logo
+          </h1>
+        </li>
+        <li className="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl">
+          <a
+            href="#"
+            className="bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white"
+          >
+            <RiHome6Line className="text-2xl" />
+          </a>
+        </li>
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <CiDiscount1 className="text-2xl" />
+          </a>
+        </li>
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <RiPieChartLine className="text-2xl" />
+          </a>
+        </li>
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <RiMailLine className="text-2xl" />
+          </a>
+        </li>
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <RiNotification2Line className="text-2xl" />
+          </a>
+        </li>
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <RiSettings5Line className="text-2xl" />
+          </a>
+        </li>
+      </ul>
     </div>
+    <div>
+      <ul className="pl-4">
+        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <a
+            href="#"
+            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+          >
+            <RiLogoutBoxRLine className="text-2xl" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
   );
 };
 
